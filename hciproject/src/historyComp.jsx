8f6cea12004e939f7835357326dba3b4632071ca
Ollie Edwards <ollie3edwards@gmail.com>
2025-12-01 11:20:13 +0000

    allow items to be deleted

--- hciproject/src/historyComp.jsx
+++ hciproject/src/historyComp.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import { RiDeleteBinLine } from "react-icons/ri";
 import { FaHistory } from "react-icons/fa";
 
-// History Component
 const HistoryOverlay = ({ onClose }) => {
+
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      img: "Images/Scan1Img.png",
+      name: "Drinks Can",
+      location: "Box 1",
+      date: "28/11/25",
+    },
+    {
+      id: 2,
+      img: "Images/Scan2Img.png",
+      name: "White Paper",
+      location: "Box 2",
+      date: "28/11/25",
+    },
+    {
+      id: 3,
+      img: "Images/Scan3Img.png",
+      name: "Sweets Wrapper",
+      location: "General Waste",
+      date: "27/11/25",
+    },
+  ]);
+
+  const deleteItem = (id) => {
+    setItems(prev => prev.filter(item => item.id !== id));
+  };
+
   return (
     <div style={{
       position: "absolute",
@@ -19,6 +48,8 @@ const HistoryOverlay = ({ onClose }) => {
       zIndex: 10,
       overflowY: "auto",
     }}>
+      
+      {/* Close Button */}
       <div
         style={{
           position: "absolute",
@@ -33,36 +64,35 @@ const HistoryOverlay = ({ onClose }) => {
         <IoMdClose size={40} onClick={onClose}/>
       </div>
 
-      <h2 style={{marginTop: 32}}>History</h2>
-      <div style={{position: "absolute", top:52, right:50}}><FaHistory size={50}/></div>
-      <div style={{fontSize:25, fontFamily:"Verdana"}}>
-        <ul style={{listStyle: "none", margin:0, padding:0}}>
-          <li className="scannedItem">
-            <img src="Images/Scan1Img.png" height="100px" alt="Drinks Can"/> 
-            <div className="scannedItemTextBox">
-              <p>Drinks Can</p>
-              <p>Box 1 <span style={{position: "relative", top:"5px", left:"135px"}}><RiDeleteBinLine size={25}/></span></p>
-              <p>28/11/25</p>
-            </div>
-          </li>
-          <li className="scannedItem">
-            <img src="Images/Scan2Img.png" height="100px" alt="White Paper"/> 
-            <div className="scannedItemTextBox">
-              <p>White Paper</p>
-              <p>Box 2 <span style={{position: "relative", top:"5px", left:"135px"}}><RiDeleteBinLine size={25}/></span></p>
-              <p>28/11/25</p>
-            </div>
-          </li>
-          <li className="scannedItem">
-            <img src="Images/Scan3Img.png" height="100px" alt="Sweets Wrapper"/> 
-            <div className="scannedItemTextBox">
-              <p>Sweets Wrapper</p>
-              <p>General Waste <span style={{position: "relative", top:"5px", left:"47px"}}><RiDeleteBinLine size={25}/></span></p>
-              <p>27/11/25</p>
-            </div>
-          </li>
+      <h2 style={{ marginTop: 32 }}>History</h2>
+      <div style={{ position: "absolute", top: 52, right: 50 }}>
+        <FaHistory size={50} />
+      </div>
+
+      <div style={{ fontSize: 25, fontFamily: "Verdana" }}>
+        <ul style={{ listStyle: "none", margin: 0, padding: 0 }}>
+          {items.map(item => (
+            <li className="scannedItem" key={item.id}>
+              <img src={item.img} height="100px" alt={item.name} />
+              <div className="scannedItemTextBox">
+                <p>{item.name}</p>
+                <p style={{ display: "flex", alignItems: "center" }}>
+                  <span>{item.location}</span>
+
+                  <span 
+                    style={{ marginLeft: "auto", cursor: "pointer" }} 
+                    onClick={() => deleteItem(item.id)}
+                  >
+                    <RiDeleteBinLine size={25} />
+                  </span>
+                </p>
+                <p>{item.date}</p>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
+
     </div>
   );
 };
